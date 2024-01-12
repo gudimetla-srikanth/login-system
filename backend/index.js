b46bfc115bb160b037ab24780eab735878bfe886
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-01-12 09:42:20 +0530

    committed

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const cors=require('cors')
 const app=express();
 const dotenv = require('dotenv')
 dotenv.config()
-app.use(cors())
 const connectDB = require('./config/database')
 const router = require('./routes/userRoutes')
 connectDB()
+app.use(cors())
 app.use(express.json())
 app.use('/api/v1/',router)
 app.listen(process.env.PORT,()=>{
